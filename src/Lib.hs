5e218d080eefb7bf433a5f0a9c537ddc3703577e
Tom Sydney Kerckhove <syd@fpcomplete.com>
2017-11-17 15:29:18 +0100

    Step 1: Sample Implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
     ( someFunc
+    , myReverse
+    , myAbs
+    , writeFileDouble
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+myReverse :: [a] -> [a]
+myReverse [a] = [a]
+myReverse (x:xs) = xs ++ [x]
+
+myAbs :: Int -> Int
+myAbs x
+    | x < 0 = -x
+    | x > 0 = x
+
+writeFileDouble :: FilePath -> String -> IO ()
+writeFileDouble path content = writeFile path $ content ++ content
