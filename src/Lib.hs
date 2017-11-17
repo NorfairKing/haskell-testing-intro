5f0ba7ce999a1d64771e205b5351e96f7461e5cb
Tom Sydney Kerckhove <syd@fpcomplete.com>
2017-11-17 16:01:01 +0100

    Step 2: Code Fixed

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,13 +9,13 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 myReverse :: [a] -> [a]
-myReverse [a] = [a]
+myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
 myAbs :: Int -> Int
 myAbs x
     | x < 0 = -x
-    | x > 0 = x
+    | x >= 0 = x
 
 writeFileDouble :: FilePath -> String -> IO ()
 writeFileDouble path content = writeFile path $ content ++ content
